030c6a2757dff4719144b8121c3f9a87837c10f9
umamanasa <umamanasa49@gmail.com>
2024-04-01 14:58:46 +0530

    setting up app

---
+++ main.tf
@@ -0,0 +1,96 @@
+resource "aws_security_group" "main" {
+  name        = "${local.name_prefix}-sg"
+  description = "${local.name_prefix}-sg"
+  vpc_id      = var.vpc_id
+  tags        = merge(local.tags, { Name = "${local.name_prefix}-sg" })
+
+  ingress {
+    description = "APP"
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_ingress_cidr
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ssh_ingress_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_launch_template" "main" {
+  name          = local.name_prefix
+  image_id      = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+  user_data = base64encode(templatefile("${path.module}/userdata.sh",
+    {
+      component = var.component
+  }))
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(local.tags, { Name = "${local.name_prefix}-ec2" })
+  }
+}
+
+resource "aws_autoscaling_group" "main" {
+  name                = "${local.name_prefix}-asg"
+  vpc_zone_identifier = var.subnet_ids
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  target_group_arns   = [aws_lb_target_group.main.arn]
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    propagate_at_launch = true
+    value               = local.name_prefix
+  }
+}
+
+resource "aws_route53_record" "main" {
+  name    = "${var.component}-${var.env}"
+  type    = "CNAME"
+  zone_id = var.zone_id
+  ttl     = 30
+  records = [var.alb_name]
+}
+
+resource "aws_lb_target_group" "main" {
+  name      = local.name_prefix
+  port      = var.port
+  protocol  = "HTTP"
+  vpc_id    = var.vpc_id
+}
+
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = aws_lb_listener.main.arn
+  priority     = var.lb_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.component}-${var.env}.manasareddy.online"]
+    }
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "component" {}
+variable "port" {}
+variable "sg_ingress_cidr" {}
+variable "ssh_ingress_cidr" {}
+variable "instance_type" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "zone_id" {}
+variable "alb_name" {}
+variable "listener" {}
+variable "lb_priority" {}
