6f44c5788e1d894bdbaea503c6fc2b31c5a9c39e
umamanasa <umamanasa49@gmail.com>
2024-04-27 13:32:58 +0530

    ami

--- data.tf
+++ data.tf
@@ -1,7 +1,13 @@
+# data "aws_ami" "ami" {
+#   most_recent = true
+#   name_regex  = "Centos-8-DevOps-Practice"
+#   owners      = ["973714476881"]
+# }
+
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex  = "expense-ami-v1"
+  owners      = ["206243364202"]
 }
 
 data "dns_a_record_set" "private_alb" {
